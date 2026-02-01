a1d670613b57f509907be3d7efbe16d6035decce
Marcelo Lima <bromolima@gmail.com>
2026-02-01 12:53:30 -0300

    fix: now GetByUrl method don't return an error when record is not found

---
+++ internal/repository/link/sqlc.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/m-bromo/atom-ly/internal/database/postgres/sqlc"
+	"github.com/m-bromo/atom-ly/internal/domain/entities"
+)
+
+type postgresLinkRepository struct {
+	querier sqlc.Querier
+}
+
+func NewPostgresLinkRepository(querier sqlc.Querier) LinkRepository {
+	return &postgresLinkRepository{
+		querier: querier,
+	}
+}
+
+func (r *postgresLinkRepository) Save(ctx context.Context, link *entities.Link) (int, error) {
+	id, err := r.querier.Save(ctx, sqlc.SaveParams{
+		Url: link.Url,
+		CreatedAt: sql.NullTime{
+			Time:  link.CreatedAt,
+			Valid: true,
+		},
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (r *postgresLinkRepository) GetByID(ctx context.Context, id int) (string, error) {
+	url, err := r.querier.GetByID(ctx, int32(id))
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
+}
+func (r *postgresLinkRepository) GetByUrl(ctx context.Context, url string) (string, error) {
+	shortCode, err := r.querier.GetByURl(ctx, url)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", nil
+		}
+
+		return "", err
+	}
+
+	return shortCode, nil
+}
