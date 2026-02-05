a1178d9dca9e9636a7661cd54c79c5efdfa5c9e4
bromolima <bromolima2@gmail.com>
2026-02-05 18:35:28 -0300

    test: Enhance ShortenLink tests and add Redirect tests

--- internal/service/link_test.go
+++ internal/service/link_test.go
@@ -47,12 +47,25 @@ func Test_ShortenLink(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "should fail when hasher fails (existing link scenario)",
+			name: "should fail when hasher fails to encode existing url",
 			args: args{
 				url: "http://example.com",
 			},
 			setupMock: func(repo *mocks.MockLinkRepository, hasher *mocks.MockHasher) {
 				repo.EXPECT().GetByUrl(mock.Anything, mock.Anything).Return(1, nil)
+				hasher.EXPECT().Encode(1).Return("", errors.New("hasher error"))
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "should fail when hasher fails to encode new url",
+			args: args{
+				url: "http://example.com",
+			},
+			setupMock: func(repo *mocks.MockLinkRepository, hasher *mocks.MockHasher) {
+				repo.EXPECT().GetByUrl(mock.Anything, mock.Anything).Return(0, nil)
+				repo.EXPECT().Save(mock.Anything, mock.Anything).Return(1, nil)
 				hasher.EXPECT().Encode(1).Return("", errors.New("hasher error"))
 			},
 			want:    "",
@@ -94,6 +107,77 @@ func Test_ShortenLink(t *testing.T) {
 			service := NewLinkService(mockLinkRepo, mockHasher)
 
 			got, err := service.ShortenLink(context.Background(), tt.args.url)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func Test_Redirect(t *testing.T) {
+	type args struct {
+		shortCode string
+	}
+
+	tests := []struct {
+		name      string
+		args      args
+		setupMock func(repo *mocks.MockLinkRepository, hasher *mocks.MockHasher)
+		want      string
+		wantErr   bool
+	}{
+		{
+			name: "should succed and return the original url",
+			args: args{
+				shortCode: "abcd123",
+			},
+			setupMock: func(repo *mocks.MockLinkRepository, hasher *mocks.MockHasher) {
+				hasher.EXPECT().Decode("abcd123").Return(1, nil)
+				repo.EXPECT().GetByID(context.Background(), 1).Return("abcd123", nil)
+			},
+			want:    "abcd123",
+			wantErr: false,
+		},
+		{
+			name: "should fail when repository fails",
+			args: args{
+				shortCode: "abcd123",
+			},
+			setupMock: func(repo *mocks.MockLinkRepository, hasher *mocks.MockHasher) {
+				hasher.EXPECT().Decode("abcd123").Return(1, nil)
+				repo.EXPECT().GetByID(context.Background(), 1).Return("", errors.New("db error"))
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "should fail when hasher fails",
+			args: args{
+				shortCode: "abcd123",
+			},
+			setupMock: func(repo *mocks.MockLinkRepository, hasher *mocks.MockHasher) {
+				hasher.EXPECT().Decode("abcd123").Return(0, errors.New("hasher error"))
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockLinkRepo := mocks.NewMockLinkRepository(t)
+			mockHasher := mocks.NewMockHasher(t)
+
+			if tt.setupMock != nil {
+				tt.setupMock(mockLinkRepo, mockHasher)
+			}
+			service := NewLinkService(mockLinkRepo, mockHasher)
+
+			got, err := service.Redirect(context.Background(), tt.args.shortCode)
 
 			if tt.wantErr {
 				assert.Error(t, err)
