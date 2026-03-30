d3f64e749148b93ecb41e1392dc14ad22f32ea1d
bromolima <bromolima@gmail.com>
2026-03-30 19:38:10 -0300

    feat: title improvment

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -8,7 +8,7 @@ interface props {
 
 export default function ({ icon, title, text }: props) {
     return (
-        <div className="size-50 bg-card flex flex-col items-center gap-4 py-6 rounded-xl">
+        <div className="size-52 bg-card flex flex-col items-center gap-4 py-6 rounded-xl">
             {icon}
 
             <h1 className="text-center font-semibold text-xl">{title}</h1>
--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -1,3 +1,11 @@
+import { Link2 } from "lucide-react";
+
 export default function () {
-    return <h1 className="text-4xl font-bold text-primary">Atomly</h1>;
+    return (
+        <div className="flex flex-row items-center gap-2">
+            <Link2 size={40} color="#0052ff" />
+
+            <h1 className="text-4xl font-bold text-primary">Atomly</h1>
+        </div>
+    );
 }
